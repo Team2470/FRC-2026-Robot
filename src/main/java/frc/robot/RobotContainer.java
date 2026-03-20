f28f30496cdc63227b286bde11197c8321a998f3
MrGotGame <jfgindt@mtu.edu>
2026-03-20 17:06:18 -0500

    Cleaned up RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,43 +5,25 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
-import static frc.robot.Constants.QuestNavConstants.QUESTNAV_STD_DEVS;
-
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.commands.FollowPathCommand;
 import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.path.PathPlannerPath;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import gg.questnav.questnav.PoseFrame;
-import gg.questnav.questnav.QuestNav;
-import gg.questnav.questnav.*;
-import frc.robot.subsystems.Hood;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Transfer;
 import frc.robot.subsystems.Turret;
 import frc.robot.subsystems.Localization.Vision;
-import frc.robot.subsystems.QuestNavSubsystem;
 import frc.robot.commands.ShooterCommand;
 
 import frc.robot.subsystems.IntakePivot;
@@ -67,8 +49,6 @@ public class RobotContainer {
     public final Transfer transfer = new Transfer();
     public final Hopper hopper = new Hopper();
     public final Intake intake = new Intake();
-    public double distanceToHub;
-    private QuestNavSubsystem questNav = new QuestNavSubsystem(drivetrain::addVisionMeasurement);
     public final Vision limelight = new Vision(drivetrain::addVisionMeasurement, () -> drivetrain.getState().Pose);
 
     public final IntakePivot intakepivot = new IntakePivot();
